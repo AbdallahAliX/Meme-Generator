642a9c760c9498597f03443b63ba3f87fa6dcc4e
Abdallah Mohamed Ali <abdallahali0004@gmail.com>
2023-09-22 16:39:23 +0300

    updates on form content

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -4,10 +4,10 @@ export default function MemeForm() {
     return(
         <div className="body-container">
             <div className="input-layout">
-                <input className="meme-input" placeholder="first-input" />
-                <input className="meme-input" placeholder="second-input" />
+                <input className="meme-input" placeholder="Top Text" />
+                <input className="meme-input" placeholder="Bottom Text" />
             </div>
-            <button className="image-generator">Get a new meme image</button>
+            <button className="image-generator">Get a new meme image 🖼</button>
         </div>
     )
 }
