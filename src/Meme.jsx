9122595acee71ce683210484531a6db9c17a5f2c
Abdallah Mohamed Ali <abdallahali0004@gmail.com>
2023-09-25 05:16:17 +0300

    MemeDate + MemeImage added

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,13 +1,30 @@
 import React from "react"
+import memesData from "./memesData"
+
+
 
 export default function MemeForm() {
+
+    const [memeImage,setMemeImage] = React.useState("");
+
+    function randomMemeImage(){
+    const memesArray = memesData.data.memes;
+    const randomImage = Math.floor(Math.random() * memesArray.length)
+    setMemeImage(memesArray[randomImage].url)
+    }
+
     return(
-        <div className="body-container">
-            <div className="input-layout">
-                <input className="meme-input" placeholder="Top Text" />
-                <input className="meme-input" placeholder="Bottom Text" />
+        <>
+            <div className="body-container">
+                <div className="input-layout">
+                    <input className="meme-input" placeholder="Top Text" />
+                    <input className="meme-input" placeholder="Bottom Text" />
+                </div>
+                <button onClick={randomMemeImage} className="image-generator">Get a new meme image 🖼</button>
+                <img src={memeImage} className="meme" />
             </div>
-            <button className="image-generator">Get a new meme image 🖼</button>
-        </div>
+            
+        </>
+
     )
 }
